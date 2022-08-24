ddbb693c18750012ec6a0dc79e7a400eaf8f0c46
sergey <he305@mail.ru>
2022-08-25 00:38:56 +0300

    Initial commit

---
+++ src/main/java/com/github/he305/contentdiscovery/ContentDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/he305/contentdiscovery/ContentDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
